6c26537f44ab6d8b507e2106272f66335c25ab59
Zac Colley <hi@zaccolley.com>
2015-07-20 19:13:36 +0100

    Some stuff i did ok

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@ var feed = new Instafeed({
     get: 'tagged',
     tagName: 'hackpompey',
     clientId: '58792a7e6884416baaf67abccd071b92',
-    resolution: 'low_resolution',
     template: '<li class="instagram-item"><a href="{{link}}">' +
                   '<span class="instagram-item__caption">{{caption}}</span>' +
                   '<img class="instagram-item__image" src="{{image}}" />' +
               '</a></li>'
 });
 feed.run();
+
+// easter eggz
+if( location.hash === '#uRaH4ck3rH4rrY' ){
+    alert("HOGWARTS AWAITS!");
+    document.body.className = 'glitch';
+}
