251ce2dd66d7202abd222c200a7dd618707a224f
Zac Colley <hi@zaccolley.com>
2015-08-19 15:17:52 +0100

    Loads of :shit:

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,16 @@ var feed = new Instafeed({
     get: 'tagged',
     tagName: 'hackpompey',
     clientId: '58792a7e6884416baaf67abccd071b92',
+    resolution: 'low_resolution',
+    limit: 14,
+    sortBy: 'random',
     template: '<li class="instagram-item"><a href="{{link}}">' +
                   '<span class="instagram-item__caption">{{caption}}</span>' +
                   '<img class="instagram-item__image" src="{{image}}" />' +
-              '</a></li>'
+              '</a></li>',
+    success: function(data){
+        var elem = document.querySelector('.instagram-feed');
+        elem.className = elem.className + " instagram-feed--shown";
+    }
 });
 feed.run();
-
-// easter eggz
-if( location.hash === '#uRaH4ck3rH4rrY' ){
-    alert("HOGWARTS AWAITS!");
-    document.body.className = 'glitch';
-}
